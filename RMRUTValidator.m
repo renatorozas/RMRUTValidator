22394e40b78c5de10384ce652ac0c675b4d03456
Renato <imexto@gmail.com>
2013-09-02 21:58:49 -0400

    RMRUTValidator class files added.

---
+++ RMRUTValidator.m
@@ -0,0 +1,43 @@
+//
+//  RMRUTValidator.m
+//  Tnek GPS
+//
+//  Created by Renato on 02-09-13.
+//  Copyright (c) 2013 renatomoya. All rights reserved.
+//
+
+#import "RMRUTValidator.h"
+
+@implementation RMRUTValidator
++ (BOOL)validateRut:(NSString *)rut
+{
+    // Get rid of the dots.
+    rut = [rut stringByReplacingOccurrencesOfString:@"." withString:@""];
+    // Get rid of the hyphen.
+    rut = [rut stringByReplacingOccurrencesOfString:@"-" withString:@""];
+    
+    NSString *numbers = [rut substringToIndex:[rut length]-1];
+    NSString *digit = [rut substringFromIndex:[rut length]-1];
+    NSArray *multipliers = @[ @2, @3, @4, @5, @6, @7, @2, @3 ];
+
+    __block int numbersSum = 0;
+    __block int index = 0;
+    
+    NSStringEnumerationOptions enumerationOptions = NSStringEnumerationReverse | NSStringEnumerationByComposedCharacterSequences;
+    
+    [numbers enumerateSubstringsInRange:NSMakeRange(0, [numbers length]) options:enumerationOptions usingBlock:^(NSString *substring, NSRange substringRange, NSRange enclosingRange, BOOL *stop) {
+        numbersSum += [substring intValue] * [multipliers[index] intValue];
+        index++;
+    }];
+    
+    int digitCalculation = 11 - (numbersSum % 11);
+
+    NSString *computedDigit = digitCalculation == 10 ? @"K" : [NSString stringWithFormat:@"%d", digitCalculation];
+    
+    if ([digit isEqualToString:computedDigit]) {
+        return YES;
+    }
+
+    return NO;
+}
+@end
